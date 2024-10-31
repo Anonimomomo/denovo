10fb7429e08f7374ee3ae42671e3390ee6e5b1cd
Anonimomomo <jean.lucas@furia.gg>
2024-10-31 11:20:06 -0300

    teste final com furia

---
+++ tilesets/pixil-frame-0 (2).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="pixil-frame-0 (2)" tilewidth="32" tileheight="32" tilecount="527" columns="31">
+ <image source="pixil-frame-0 (2).png" width="1000" height="563"/>
+</tileset>
